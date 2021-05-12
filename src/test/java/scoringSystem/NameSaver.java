925e7dcecda72d8af302d5d5c2fce186dc1f2c82
TheSaintGit <79902520+TheSaintGit@users.noreply.github.com>
2021-05-12 17:12:12 +0200

    Add files via upload

---
+++ src/test/java/scoringSystem/NameSaver.java
@@ -0,0 +1,38 @@
+package scoringSystem;
+
+import java.security.PrivateKey;
+
+public class NameSaver {
+
+        private  String[] athlets;
+        private String contest;
+        public void SetContest (String ContestName){
+            contest=ContestName;}
+
+
+        public void setAthlets(int NumberOfAthlets) {
+
+            if (NumberOfAthlets >40){
+                NumberOfAthlets=40;
+
+            }
+            athlets = new String[NumberOfAthlets];
+        }
+
+        public void setName(int place, String atheltesName) {
+
+            athlets[place] = atheltesName;
+        }
+
+    public String getContest() {
+
+        return contest;
+    }
+
+
+
+        public String [] getNames() {
+
+            return athlets;
+        }
+    }
---
+++ src/test/java/scoringSystem/SetScore.java
@@ -0,0 +1,123 @@
+package scoringSystem;
+
+import java.util.Scanner;
+
+import static scoringSystem.Main.nameSaver;
+
+public class SetScore {
+    static Scanner scan = new Scanner(System.in);
+    static String event;
+    static double result;
+
+    public static void setScore() {
+        String[] names = nameSaver.getNames();
+        String[][] Scorecard = new String[names.length + 1][22];
+        Scorecard[0][0] = "Participants";                //Scorecard [1][0]  Name of first participants
+        Scorecard[0][1] = "100m Result";                //Scorecard [1][1]  for adding 100m result for participants number 1
+        Scorecard[0][2] = "100m Points";
+        Scorecard[0][3] = "Long jump Result";           //Scorecard [1][3]  for adding Long jump result for participants number 1
+        Scorecard[0][4] = "Long jump Points";
+        Scorecard[0][5] = "Shot put Result";            //Scorecard [1][5]  for adding Shot put result for participants number 1
+        Scorecard[0][6] = "Shot put Points";
+        Scorecard[0][7] = "High jump Result";           //Scorecard [1][7]  for adding Shot put result for participants number 1
+        Scorecard[0][8] = "High jump Points";
+        Scorecard[0][9] = "400m Result";
+        Scorecard[0][10] = "400m Points";
+        Scorecard[0][11] = "110m hurdles Result";
+        Scorecard[0][12] = "110m hurdles Points";
+        Scorecard[0][13] = "Discus throw Result";
+        Scorecard[0][14] = "Discus throw Points";
+        Scorecard[0][15] = "Pole vault Result";
+        Scorecard[0][16] = "Pole vault Points";
+        Scorecard[0][17] = "Javelin throw Result";
+        Scorecard[0][18] = "Javelin throw Points";
+        Scorecard[0][19] = "1500m Result";
+        Scorecard[0][20] = "1500m Points";
+        Scorecard[0][21] = "Total Points";
+        System.err.println("Dagens tävling " + nameSaver.getContest());
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+            Scorecard[i + 1][0] = names[i];
+        }
+
+        // Hämta uträkning 100m och Längd ändra j<2 för antal grenar
+        for (int j = 0; j < 2; j++) {
+            int k = j + 1;
+            for (int i = 0; i < names.length; i++) {
+
+                switch (j) {
+                    case 0 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i sekunder på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "100m";
+                    }
+                    case 1 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i centimeter på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "Long jump";
+                    }
+                    case 2 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i meter på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "Shot put";
+                    }
+                    case 3 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i centimeter på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "High jump";
+                    }
+                    case 4 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i sekunder på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "400m";
+                    }
+                    case 5 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i sekunder på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "110m hurdles";
+                    }
+                    case 6 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i meter på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "Discus throw";
+                    }
+                    case 7 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i centimeter på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "Pole vault";
+                    }
+                    case 8 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i meter på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "Javelin throw";
+                    }
+                    case 9 -> {
+                        System.out.println(Scorecard[0][j + k] + " Write resultat i sekunder på  " + names[i]);
+                        result = scan.nextDouble();
+                        event = "1500m";
+                    }
+                }
+
+                DecaCount ScoreDeca = new DecaCount();
+                Scorecard[i + 1][j + k] = String.valueOf(result);
+                Scorecard[i + 1][j + k + 1] = String.valueOf(ScoreDeca.decaCount(event, result));
+            }
+        }
+
+       for (int l = 0; l < 22; l++) {
+            System.out.print("| " + Scorecard[0][l] + " |");                           // Skriver ut rubrikerna!
+        }
+        System.out.println();
+        for (int k = 1; k < names.length + 1; k++) {                                   // Skriver ut scorecard
+            for (int j = 0; j < 22; j++) {
+                if (Scorecard[k][j] == null) {
+                } else {
+                    System.out.print("  | " + Scorecard[k][j] +"     | ");
+                }
+            }
+            System.out.println("   ");
+        }
+    }
+
+}
